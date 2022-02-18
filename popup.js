2c9d28e9798817b39f2c1bd4e7520ff3f11fd5d3
dawg <dawid.golebiewski@tuhh.de>
2022-02-18 12:24:35 +0100

    Initial Commit. Google Docs Boilerplate with modifications. PoC successful.

---
+++ popup.js
@@ -0,0 +1,32 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("activate");
+
+// changeColor.style.backgroundColor = color;
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageReadable into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageReadable,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageReadable() {
+//  let remo1 = document.getElementById("checkout-container");
+  // document.getElementById("view-offer").style.filter = "blur(10px)";
+  // document.querySelectorAll("*[id^=offer]").remove();
+//  let chang1 = document.querySelectorAll("*[id^=offer]");
+  let dellist = document.getElementsByClassName("paywall")[0].style.filter = "blur(0px)"; //.style.filter = "blur(0px)";
+  // OBSOLETE delete = dellistp[0];
+  // chrome.storage.sync.get("color", ({ color }) => {
+  //   document.body.style.backgroundColor = color;
+  // });
+}
