2bd8c7d37abe4424a08a9d56f382c4d145169c21
dawg <dawid.golebiewski@tuhh.de>
2022-02-18 12:51:36 +0100

    Selecting the elements that render the modal from the DOM fails - no idea why. It is like they are invisible from the console even though I can clearly see them in the source of the page

--- popup.js
+++ popup.js
@@ -20,6 +20,9 @@ changeColor.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageReadable() {
-  document.getElementsByClassName("view view-offer")[0].remove()
+  // document.getElementsByTagName("custom-script")[0].remove()
   document.getElementsByClassName("paywall")[0].style.filter = "blur(0px)";
+  // document.getElementById("checkout-container").remove();
+  // document.getElementsByClassName("lazy-transclude")[0].remove();
+  //let removablelist = document.getElementsByClassName("view view-offer")[0].remove();
 }
